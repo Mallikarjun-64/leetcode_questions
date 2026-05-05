c10c125f1471d1e87744c57a713e80baa1d5a2d3
Mallikarjun-64 <kgoudar.ms@gmail.com>
2026-05-05 21:42:40 +0530

    valid anagram

---
+++ ValidAnagram_242.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char arr[] = t.toCharArray();
+        char arr1[] = s.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
+        String s1=new String(arr);
+        String s2=new String(arr1);
+
+        return s1.equals(s2);
+
+    }
+}
