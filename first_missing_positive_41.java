f56722e2e6c9d595df2ea10e074652c11313b7e1
Mallikarjun-64 <kgoudar.ms@gmail.com>
2026-05-23 13:57:42 +0530

    first missing positive 41

---
+++ first_missing_positive_41.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer> set= new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        Arrays.sort(nums);
+        for(int i=1;i<=nums.length;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }return nums[nums.length-1]+1;
+    }
+}
